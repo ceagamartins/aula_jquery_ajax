d6f6a565b06b7566026412baaa210b8a06bd2bd3
Christian Martins <martins.christian@live.com>
2020-03-20 10:32:44 -0300

    Implementando Boostrap ao HTML e criando interações com JQuery/Ajax

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //alert("funcionou cara")
 
 function consultaCep(){
+    $(".barra-progresso").show()
     var cep = document.getElementById("cep").value
     var url = "http://viacep.com.br/ws/" + cep + "/json/"
     console.log(url)
@@ -9,14 +10,21 @@ function consultaCep(){
         type: "GET",
         success: function(response){
             console.log(response)
-            /* $("#logradouro").html(response.logradouro)
+            $("#logradouro").html(response.logradouro)
             //document.getElementById("logradouro").innerHTML = response.logradouro
             $("#bairro").html(response.bairro)
             //document.getElementById("bairro").innerHTML = response.bairro
             $("#localidade").html(response.localidade)
             //document.getElementById("localidade").innerHTML = response.localidade
             $("#uf").html(response.uf)
-            //document.getElementById("uf").innerHTML = response.uf */
+            //document.getElementById("uf").innerHTML = response.uf
+            $("#titulo_cep").html("CEP " + response.cep)
+            $(".cep").show()
+            $(".barra-progresso").hide()
         }
     })
 }
+$(function(){
+    $(".cep").hide()
+    $(".barra-progresso").hide()
+})
